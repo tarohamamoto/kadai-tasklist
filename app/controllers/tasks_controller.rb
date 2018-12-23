f00c84986ea5553739e213fe46a88e5ddf166cc3
taro <thama8823@gmail.com>
2018-12-23 03:59:09 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     
-    if @@task.update(task_params)
+    if @task.update(task_params)
       flash[:success] = 'Taskは正常に更新されました'
       redirect_to @task
     else
